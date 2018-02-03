ead907f96975a93e59619073f9ed73da0e34c598
ShiveryMoon <yuanyuehang2006@163.com>
2018-02-03 18:28:59 +0800

    update

--- 3/用快排求数组中第N大元素.py
+++ 3/用快排求数组中第N大元素.py
@@ -3,7 +3,7 @@ from random import randint
 #此算法的时间复杂度为On
 
 def quickSortFindN(alist,n):
-    N = quickSortHelper(alist,0,len(alist)-1,n-1)
+    N = quickSortHelper(alist,0,len(alist)-1,len(alist)-n)
     return N
 
 def quickSortHelper(alist,first,last,n):
@@ -36,6 +36,6 @@ def partition(alist,first,last):
     alist[first],alist[rightmark]=alist[rightmark],alist[first]
     return rightmark
 
-list=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-N=quickSortFindN(list,11)
+list=[2,1]
+N=quickSortFindN(list,1)
 print(N)
